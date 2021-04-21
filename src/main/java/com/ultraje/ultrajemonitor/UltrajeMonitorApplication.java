be6c42af5eeb20fae84b5aaf50d73ac6abad9e3d
Rafael Amorim <ramorim@bionexo.com>
2021-04-21 12:06:08 -0300

    subidno

---
+++ src/main/java/com/ultraje/ultrajemonitor/UltrajeMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.ultraje.ultrajemonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UltrajeMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UltrajeMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ultraje/ultrajemonitor/UltrajeMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ultraje.ultrajemonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UltrajeMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
